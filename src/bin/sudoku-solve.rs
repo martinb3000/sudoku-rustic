2f17bdce821e2cc8452603d52198d1f73cbcbb75
Martin Bergström <marbe@bredband.net>
2021-09-12 22:13:36 +0200

    Allow specifying maximum number of solutions desired.

--- src/bin/sudoku-solve.rs
+++ src/bin/sudoku-solve.rs
@@ -1,18 +1,24 @@
+use std::env;
 use std::error::Error;
 use std::io::{self, Read};
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-    let maximum_solutions = 1; // TODO: Parse argument.
+    let mut maximum_solutions = 1;
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        maximum_solutions = args[1].parse()?;
+    }
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
     let grid = sudokur::parse(&buffer)?;
     let solutions = sudokur::solutions(&grid)?;
     for (i, solution) in solutions.enumerate().take(maximum_solutions) {
         if i > 0 {
-            println!("\n == Solution {} ==\n", i + 1);
+            println!("\n == Solution {} ==", i + 1);
         }
         let formatted = sudokur::format(solution);
         print!("{}", formatted);
     }
     Ok(())
 }
+
